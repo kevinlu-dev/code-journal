f69b794263b7b9adf796232aea48afa61046fada
Kevin Lu <kevinlu.dev@gmail.com>
2022-07-19 09:04:50 +0000

    up to submit button, only prepend subtask left. commented out code to commit and save, due to consolelog and unused variable not allowing for commits otherwise.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*
+var inputPhotoUrl = document.getElementById('user-photo-url');
+var submitJournalEntry = document.getElementById('code-journal-form');
+
+ console.log(submitJournalEntry);
+
+inputPhotoUrl.addEventListener('input', function (event) {
+  document.getElementById('image-preview').src = event.target.value;
+});
+
+submitJournalEntry.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var journalAll = {
+    title: submitJournalEntry.elements.title.value,
+    photoUrl: submitJournalEntry.elements.photourl.value,
+    notes: submitJournalEntry.elements.notes.value,
+    entryId: data.nextEntryId++
+  };
+  console.log(journalAll);
+  document.getElementById('code-journal-form').reset();
+}); */
